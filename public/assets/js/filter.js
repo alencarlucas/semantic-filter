b9ce0ec2ed6284264b7e15daf5be7545b27a5b89
alencarlucas <alencarlucaas@gmail.com>
2017-07-13 09:02:18 -0300

    Multiple especialidad filter and responsive layout fixed

--- public/assets/js/filter.js
+++ public/assets/js/filter.js
@@ -1,37 +1,39 @@
+//Variáveis para armazenar os dados dos filtros
 var sectorFilter = '';
 var especialidadFilter = '';
 var nombre = '';
 var sede = '';
 
+//Array de objects utilizado apenas para testes
 var data = [
-  {nombre : 'Carlos', sede : 'somewhere', especialidad : 'Derecho de prueba', sector : 'valencia'},
+  {nombre : 'Carlos', sede : 'somewhere', especialidad : ['Derecho de prueba', 'Internet'], sector : 'Valencia'},
+  {nombre : 'João', sede : 'somewhere', especialidad : ['Derecho Igualitario'], sector : 'Valencia'},
+  {nombre : 'Maria', sede : 'somewhere', especialidad : ['Derecho societario', 'Derecho Mercantil'], sector : 'Zaragosa'},
+  {nombre : 'Carlos', sede : 'somewhere', especialidad : ['Derecho societario', 'Derecho Igualitario'], sector : 'Valencia'},
 ];
 
-$('#especialidad')
-.dropdown({
-    onChange: function(value, text, $selectedItem) {
-        programFilter = value;
-    }
-});
+//Inicializa os dropdowns
+$('#especialidad').dropdown();
 
-$('#sede')
-  .dropdown()
-;
+$('#sede').dropdown();
 
-$('#sector')
-  .dropdown()
-;
+$('#sector').dropdown();
 
+//Atribui os valores de entrada dos filtros às variáveis e faz a busca
 $(function() {
     document.getElementById('buscar').addEventListener('click', function() {
-        sectorFilter = document.getElementById('sector').value;
-        especialidadFilter = document.getElementById('especialidad').value;
-        nombreFilter = document.getElementById('nombre').value;
-        sedeFilter = document.getElementById('sede').value;
+        sectorFilter = $('#sector').val();
+        especialidadFilter = $('#especialidad').val();
+        nombreFilter = $('#nombre').val();
+        sedeFilter = $('#sede').val();
         search();
     });
 });
 
+/*
+    Testa os filtros para cada entrada de dados e se encontrou a referência
+    apresenta o nome no console
+*/
 function search(){
     for(i in data){
         if(execFilter(data[i]))
@@ -39,8 +41,17 @@ function search(){
   }
 }
 
+//Verifica a referência de todas as especialidades filtradas
+function verifyEspecialidad(arrayEspecialidad){
+    for(j in arrayEspecialidad)
+        if(especialidadFilter.indexOf(arrayEspecialidad[i]) < 0)
+            return 1;
+    return 0;
+}
+
+//Testa aderência aos filtros
 function execFilter(row) {
-    if ((especialidadFilter != '') && especialidadFilter.indexOf(row['especialidad']) < 0)
+    if ((especialidadFilter != '') && verifyEspecialidad(row['especialidad']))
         return 0;
     if (sectorFilter != '' && sectorFilter != row['A_C_Operator_Manager__c'])
         return 0;
